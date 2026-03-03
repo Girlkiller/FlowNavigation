8132e0124c79156460e9a61c19a2b79c27622e54
书香阁锦衣剑 <137019550@qq.com>
2026-03-03 13:22:47 +0800

    Initial Commit

---
+++ FlowNavigation/FlowNavigation.swift
@@ -0,0 +1,9 @@
+//
+//  FlowNavigation.swift
+//  FlowNavigation
+//
+//  Created by feng qiu on 2026/3/3.
+//
+
+import Foundation
+
---
+++ FlowNavigationTests/FlowNavigationTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowNavigationTests.swift
+//  FlowNavigationTests
+//
+//  Created by feng qiu on 2026/3/3.
+//
+
+import Testing
+@testable import FlowNavigation
+
+struct FlowNavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
